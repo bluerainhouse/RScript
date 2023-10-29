0874a0de93cd68ccafd193f7ab94bbb88da7a1a4
bluerainhouse <samba255256@gmail.com>
2023-10-29 12:10:12 +0800

    initial

---
+++ HW1.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+product_list <- read.csv("product_list.csv")
+product_list <- product_list %>%
+  separate(Item, into=c("Product", "Item"), sep = "_", convert = TRUE)
+
+client_list <- read.csv("client_list.csv")
+salesdata <- read.csv("salesdata.csv")
+
+client_list <- client_list %>% select(!(X))
+salesdata <- salesdata %>% select(!(X))
+
+result <- salesdata %>%
+  left_join(client_list, by = "Client") %>%
+  left_join(product_list, by = "Product")
